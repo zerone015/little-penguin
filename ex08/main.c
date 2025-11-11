83a9814eba0f3f3219eaa108e90a4ffb2400876b
Son Young Won <zerone015@naver.com>
2025-11-12 00:01:19 +0900

    feat: ex08

---
+++ ex08/main.c
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0
+ #include <linux/module.h>
+ #include <linux/kernel.h>
+ #include <linux/init.h>
+ #include <linux/miscdevice.h>
+ #include <linux/fs.h>
+ #include <linux/slab.h>
+ #include <linux/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
+MODULE_DESCRIPTION("Reverse misc device");
+
+static ssize_t myfd_read(struct file *fp, char __user *user,
+			 size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user,
+			  size_t size, loff_t *offs);
+
+static const struct file_operations myfd_fops = {
+	.owner = THIS_MODULE,
+	.read  = myfd_read,
+	.write = myfd_write,
+};
+
+static struct miscdevice myfd_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = "reverse",
+	.fops  = &myfd_fops,
+};
+
+static char str[PAGE_SIZE];
+
+static int __init myfd_init(void)
+{
+	int retval;
+
+	retval = misc_register(&myfd_device);
+	if (retval) {
+		pr_err("failed to register misc device\n");
+		return retval;
+	}
+	pr_info("/dev/reverse registered\n");
+	return 0;
+}
+
+static void __exit myfd_cleanup(void)
+{
+	misc_deregister(&myfd_device);
+	pr_info("/dev/reverse unregistered\n");
+}
+
+static ssize_t myfd_read(struct file *fp, char __user *user,
+			 size_t size, loff_t *offs)
+{
+	size_t t, i, len;
+	char *tmp;
+
+	len = strlen(str);
+	if (!len)
+		return 0;
+	tmp = kmalloc(len + 1, GFP_KERNEL);
+	if (!tmp)
+		return -ENOMEM;
+	for (t = len - 1, i = 0; t < len; t--, i++)
+		tmp[i] = str[t];
+	tmp[i] = '\0';
+	i = simple_read_from_buffer(user, size, offs, tmp, len);
+	kfree(tmp);
+	return i;
+}
+
+static ssize_t myfd_write(struct file *fp, const char __user *user,
+			  size_t size, loff_t *offs)
+{
+	ssize_t res;
+
+	res = simple_write_to_buffer(str, PAGE_SIZE, offs, user, size);
+	if (res < 0)
+		return res;
+	str[res] = '\0';
+	return res;
+}
+
+module_init(myfd_init);
+module_exit(myfd_cleanup);
